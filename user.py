8f514d66b11914f6b0fe0c0d5a1d61fc8392fab8
Jose Sejas Escobar <153658903+JoseEscobar19@users.noreply.github.com>
2026-01-04 02:44:12 -0500

    Update user.py

--- user.py
+++ user.py
@@ -4,9 +4,20 @@
 def login(database, username, password):
     if username in database and database[username] == password:
         print(f"Welcome back {username}!")
+        return username
     elif username in database and database[username] != password:
         print("Password incorrect")
         return ""
     else:
         print("Username not found")
         return ""
+    
+#task5: Register functionality
+
+def register(database, username):
+    if username in database:
+        print("Username already registered")
+        return ""
+    else:
+        print(f"Username {username} registered!")
+        return username
