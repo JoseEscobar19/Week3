4241920cd685e3099d88b034cfa7322df44e3331
Jose Sejas Escobar <153658903+JoseEscobar19@users.noreply.github.com>
2026-01-04 02:43:31 -0500

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -8,3 +8,22 @@ def show_homepage():
     print("|            5.     Exit                 |")
     print("------------------------------------------")
 
+#task6, donate functionality
+
+def donate(username):
+    donation_amt = input("Enter amount to donate")
+    donation_string = f"{username} donated ${donation_amt}"
+    print(donation_string)
+    print("thank you for your donation!")
+    return donation_string
+
+#task7, show donations functionality
+
+def show_donations(donations):
+    print("\n---All Donations---")
+    if donations == []:
+        print("there are currently no donations")
+    else:
+        for item in donations:
+            print(item)
+            
